c712b340bf59b449fc56d0d7156e96be22d379a5
jimwilmes32 <46799908+jimwilmes32@users.noreply.github.com>
2019-01-17 19:08:13 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,21 @@
+/**
+ * Author: Mike and Jim Wilmes 
+ * Date: 2019/01/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jim Wilmes\n");
+  
+  printf("I am a Mechanical Engineering Major\n");
+  
+  
+  
+
+  return 0;
+}
